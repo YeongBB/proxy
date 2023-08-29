17e0ccfa55206a45162ce55c221f59e8bfd7d39e
mac <handfaste@naver.com>
2023-08-29 17:30:19 +0900

    BeanPostProcessor

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,8 @@ package hello.proxy;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_orixyfactory.ProxyFactoryConfig2;
 import hello.proxy.config.v3_orixyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +19,10 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyFilterConfig.class)
 
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfig2.class)
+//@Import(ProxyFactoryConfig2.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
+
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
